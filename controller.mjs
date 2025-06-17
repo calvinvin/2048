d7619910927327f8ee283e812ff7807cdc1b6cae
Calvin <calvinvin@gmail.com>
2025-06-17 15:38:46 +0800

    ver 1.1.3 on 2025-06-17. 修正觸控控制未觸發checkGameOver().

--- controller.mjs
+++ controller.mjs
@@ -8,24 +8,98 @@ export class GameController {
     this.view = new View(this.gameContainerElement);
   }
 
-  attachKeyboardControl() {
-    window.addEventListener("keydown", (e) => {
-      e.preventDefault();
-      // if (!Object.keys(keyDownDirectionDict).includes(key)) return;
-      if (this.game.gameOver) return;
-      this.game.move(this.#pressedKeyToDirection(e));
-      this.view.updateView(this.game.gameDataArray, this.game.gameRoundRecords);
-      if (this.game.isEndGame(this.game.gameDataArray)) {
-        this.game.gameOver = true;
-        setTimeout(() => window.alert("End game!"), 500);
-      }
-    });
-  }
-
   initialize() {
     this.__initializeHTMLElements();
     this.__initializeGameBoard();
   }
+
+  restartGame() {
+    this.game.round++;
+    this.game.gameRoundRecords = [];
+    this.game.gameDataArray = [...Array(16)].map((_) => null);
+    this.game.gameOver = false;
+    this.game.gameDatumIdGenerator = this.game.gameDatumIdGeneratorFunction();
+    this.view.updateView(this.game.gameDataArray, [null]);
+    this.game.createNewGameDatum();
+    this.game.createNewGameDatum();
+    this.game.gameRoundRecords.push(this.game.gameDataArray);
+    this.view.updateView(this.game.gameDataArray, this.game.gameRoundRecords);
+  }
+
+  attachKeyboardControl() {
+    window.addEventListener("keydown", (e) => {
+      e.preventDefault();
+      if (this.game.gameOver) return;
+      this.game.move(this.#pressedKeyToDirection(e));
+      this.view.updateView(this.game.gameDataArray, this.game.gameRoundRecords);
+      this.#checkEndGame();
+    });
+  }
+  #checkEndGame() {
+    if (this.game.isEndGame(this.game.gameDataArray)) {
+      this.game.gameOver = true;
+      setTimeout(() => window.alert("End game!"), 500);
+      return true;
+    }
+    return false;
+  }
+
+  attachSwipeControl() {
+    const swipeThreshold = 25;
+    const swipeDirectionRatioThreshold = 1.5;
+    let initialX, initialY, endX, endY;
+    let moved = false;
+    const pointerDownHandler = handlePointerDown.bind(this);
+    const pointerMoveHandler = handlePointerMove.bind(this);
+    const pointerUpHandler = handlePointerUp.bind(this);
+    document.addEventListener("pointerdown", pointerDownHandler);
+    document.addEventListener("pointerup", pointerUpHandler);
+
+    function handlePointerDown(e) {
+      initialX = e.screenX;
+      initialY = e.screenY;
+      document.addEventListener("pointermove", pointerMoveHandler);
+    }
+    function handlePointerMove(e) {
+      endX = e.screenX;
+      endY = e.screenY;
+      const swipeDirection = getSwipeDirection(initialX, initialY, endX, endY);
+      if (swipeDirection && !moved) {
+        this.game.move(swipeDirection);
+        this.view.updateView(
+          this.game.gameDataArray,
+          this.game.gameRoundRecords
+        );
+        moved = true;
+        if (this.#checkEndGame()) {
+          document.removeEventListener("pointerdown", pointerDownHandler);
+          document.removeEventListener("pointerup", pointerUpHandler);
+        }
+      }
+    }
+    function handlePointerUp(e) {
+      document.removeEventListener("pointermove", pointerMoveHandler);
+      moved = false;
+    }
+
+    function getSwipeDirection(initialX, initialY, endX, endY) {
+      const diffX = endX - initialX;
+      const diffY = endY - initialY;
+      const absDiffX = Math.abs(diffX);
+      const absDiffY = Math.abs(diffY);
+      if (![absDiffX, absDiffY].some((absDiff) => absDiff > swipeThreshold)) {
+        return null;
+      }
+      if (absDiffX / absDiffY > swipeDirectionRatioThreshold) {
+        return diffX > 0 ? "right" : "left";
+      } else if (absDiffY / absDiffX > swipeDirectionRatioThreshold) {
+        return diffY > 0 ? "down" : "up";
+      } else {
+        return null;
+      }
+    }
+  }
+
   #pressedKeyToDirection(e) {
     const keyDownDirectionDict = {
       arrowleft: "left",
@@ -80,68 +154,5 @@ export class GameController {
     this.gameContainerElement.appendChild(moveStepsDiv);
     this.gameContainerElement.appendChild(gameBoardContainer);
     this.gameContainerElement.appendChild(restartButton);
-  }
-
-  restartGame() {
-    this.game.round++;
-    this.game.gameRoundRecords = [];
-    this.game.gameDataArray = [...Array(16)].map((_) => null);
-    this.game.gameOver = false;
-    this.game.gameDatumIdGenerator = this.game.gameDatumIdGeneratorFunction();
-    this.view.updateView(this.game.gameDataArray, [null]);
-    this.game.createNewGameDatum();
-    this.game.createNewGameDatum();
-    this.game.gameRoundRecords.push(this.game.gameDataArray);
-    this.view.updateView(this.game.gameDataArray, this.game.gameRoundRecords);
-  }
-
-  attachSwipeControl() {
-    const swipeThreshold = 25;
-    const swipeDirectionRatioThreshold = 1.5;
-    const listeningMove = handlePointerMove.bind(this);
-    let initialX, initialY, endX, endY;
-    let moved = false;
-    document.addEventListener("pointerdown", handlePointerDown);
-    document.addEventListener("pointerup", handlePointerUp.bind(this));
-
-    function handlePointerDown(e) {
-      initialX = e.screenX;
-      initialY = e.screenY;
-      document.addEventListener("pointermove", listeningMove);
-    }
-    function handlePointerMove(e) {
-      endX = e.screenX;
-      endY = e.screenY;
-      const swipeDirection = getSwipeDirection(initialX, initialY, endX, endY);
-      if (swipeDirection && !moved) {
-        this.game.move(swipeDirection);
-        this.view.updateView(
-          this.game.gameDataArray,
-          this.game.gameRoundRecords
-        );
-        moved = true;
-      }
-    }
-    function handlePointerUp(e) {
-      document.removeEventListener("pointermove", listeningMove);
-      moved = false;
-    }
-
-    function getSwipeDirection(initialX, initialY, endX, endY) {
-      const diffX = endX - initialX;
-      const diffY = endY - initialY;
-      const absDiffX = Math.abs(diffX);
-      const absDiffY = Math.abs(diffY);
-      if (![absDiffX, absDiffY].some((absDiff) => absDiff > swipeThreshold)) {
-        return null;
-      }
-      if (absDiffX / absDiffY > swipeDirectionRatioThreshold) {
-        return diffX > 0 ? "right" : "left";
-      } else if (absDiffY / absDiffX > swipeDirectionRatioThreshold) {
-        return diffY > 0 ? "down" : "up";
-      } else {
-        return null;
-      }
-    }
   }
 }
